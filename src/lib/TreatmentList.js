3835161aca7abbb8ea3136f0da441c28101cdb80
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-07-29 23:11:57 +0530

    feat: add detailed description to treatment cards

---
+++ src/lib/TreatmentList.js
@@ -0,0 +1,58 @@
+export const treatments = [
+  {
+    "name": "Teeth Cleaning",
+    "description": "Regular dental cleaning to remove plaque and tartar buildup for healthier teeth and gums.",
+    "intro": "Regular dental cleaning to remove plaque and tartar buildup.",
+    "detailedDescription": "Regular dental cleaning is a preventive dental procedure that helps remove plaque and tartar from the teeth and gums. It involves scaling and polishing to keep your teeth and gums healthy.",
+    "image": "./images/treatments/Teeth-cleaning.jpg"
+  },
+  {
+    "name": "Teeth Whitening",
+    "description": "Professional teeth whitening to brighten your smile and remove stains.",
+    "intro": "Professional teeth whitening to brighten your smile.",
+    "detailedDescription": "Teeth whitening is a cosmetic dental procedure that helps remove stains and discoloration from the teeth, resulting in a brighter and whiter smile. It is a safe and effective way to enhance your smile's appearance.",
+    "image": "./images/treatments/Teeth-Whitening.jpg"
+  },
+  {
+    "name": "Orthodontic Treatment",
+    "description": "Alignment of teeth using braces or aligners to improve the bite and aesthetics.",
+    "intro": "Alignment of teeth using braces or aligners.",
+    "detailedDescription": "Orthodontic treatment is used to correct the alignment of teeth and jaws. It involves the use of braces or clear aligners to gradually move the teeth into their proper positions, improving both the bite and aesthetics.",
+    "image": "./images/treatments/Orthodontics.jpg"
+  },
+  {
+    "name": "Dental Implants",
+    "description": "Permanent replacement for missing teeth that look and function like natural teeth.",
+    "intro": "Permanent replacement for missing teeth.",
+    "detailedDescription": "Dental implants are a long-lasting and natural-looking solution for replacing missing teeth. The implant is surgically placed into the jawbone, where it fuses with the bone, providing a strong foundation for the replacement tooth.",
+    "image": "./images/treatments/Dental-Implant.jpg"
+  },
+  {
+    "name": "Dental Crowns",
+    "description": "Custom-made tooth-shaped caps to restore damaged or weak teeth.",
+    "intro": "Custom-made tooth-shaped caps.",
+    "detailedDescription": "Dental crowns, also known as caps, are prosthetic devices used to restore damaged or weak teeth. They cover the entire visible portion of the tooth, providing strength and improving its appearance.",
+    "image": "./images/treatments/Dental-Crowns.jpg"
+  },
+  {
+    "name": "Dental Fillings",
+    "description": "Restoration of cavities with dental fillings to prevent further decay.",
+    "intro": "Restoration of cavities with dental fillings.",
+    "detailedDescription": "Dental fillings are used to restore teeth that have been damaged by decay. The procedure involves removing the decayed portion of the tooth and filling it with a durable material, such as composite resin or amalgam, to prevent further decay and restore the tooth's function.",
+    "image": "./images/treatments/Tooth-Filling.jpg"
+  },
+  {
+    "name": "Root Canal Therapy",
+    "description": "Treatment to save and repair an infected or decayed tooth, avoiding extraction.",
+    "intro": "Treatment to save and repair an infected tooth.",
+    "detailedDescription": "Root canal therapy, also known as endodontic treatment, is a procedure used to save and repair a badly infected or decayed tooth. The treatment involves removing the infected pulp, cleaning the root canals, and sealing them to prevent further infection.",
+    "image": "./images/treatments/Root-Canal-Treatment.jpg"
+  },
+  {
+    "name": "Dental Bridges",
+    "description": "Fixed prosthetic to replace missing teeth and restore your smile's appearance.",
+    "intro": "Fixed prosthetic to replace missing teeth.",
+    "detailedDescription": "Dental bridges are fixed prosthetic devices used to replace one or more missing teeth. They are anchored to the adjacent natural teeth or dental implants, providing a stable and natural-looking replacement for the missing teeth.",
+    "image": "./images/treatments/Dental-Bridge.jpg"
+  },
+];
--- src/pages/Treatments.js
+++ src/pages/Treatments.js
@@ -1,85 +1,99 @@
-import React from 'react'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { treatments } from "../lib/TreatmentList";
+import { Button } from "@mui/material";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { WHATSAPP_ID } from "../lib/userInfo";
+import { Breadcrumbs } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import { Link } from "react-router-dom";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 function Treatments() {
+  const [expanded, setExpanded] = React.useState(-1);
+
+  const handleExpandClick = (index) => {
+    setExpanded(expanded === index ? -1 : index);
+  };
+
   return (
-    <div className='p-5 flex flex-wrap gap-5 justify-center'>
-        {treatments.map((treatment) => (
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image={treatment.image}
-                    alt={treatment.name}
-                    className='h-48'
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {treatment.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {treatment.description}
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Consult a doctor
-                    </Button>
-                </CardActions>
-            </Card>
+    <div className="px-5 md:px-10 py-5">
+      <Toolbar className="flex flex-col pb-5">
+        <h2 className="text-3xl font-serif mr-auto">Treatments</h2>
+        <div className=" mr-auto">
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link to="/">Home</Link>
+            <Typography color="text.primary">Treatments</Typography>
+          </Breadcrumbs>
+        </div>
+      </Toolbar>
+      <div className="p-5 flex flex-wrap gap-5 justify-center bg-sky-100">
+        {treatments.map((treatment, index) => (
+          <Card key={index} sx={{ maxWidth: 345 }} className="mb-auto ">
+            <CardMedia
+              component="img"
+              image={treatment.image}
+              alt={treatment.name}
+              className="h-56"
+            />
+            <CardContent>
+              <Typography variant="body2">{treatment.name}</Typography>
+              <Typography variant="body2" color="text.secondary">
+                {treatment.description}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <a href={WHATSAPP_ID} target="blank">
+                <Button>
+                  Consult Now &nbsp;
+                  <WhatsAppIcon />
+                </Button>
+              </a>
+              <ExpandMore
+                expand={index === expanded}
+                onClick={() => handleExpandClick(index)}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={index === expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>
+                  {treatment.detailedDescription}
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
         ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Treatments
-
-const treatments = [
-      {
-        "name": "Teeth Cleaning",
-        "description": "Regular dental cleaning to remove plaque and tartar buildup for healthier teeth and gums.",
-        "image": "https://source.unsplash.com/random/250x250/?teeth_cleaning"
-      },
-      {
-        "name": "Teeth Whitening",
-        "description": "Professional teeth whitening to brighten your smile and remove stains.",
-        "image": "https://source.unsplash.com/random/250x250/?teeth_whitening"
-      },
-      {
-        "name": "Dental Fillings",
-        "description": "Restoration of cavities with dental fillings to prevent further decay.",
-        "image": "https://source.unsplash.com/random/250x250/?teeth_decay"
-      },
-      {
-        "name": "Dental Implants",
-        "description": "Permanent replacement for missing teeth that look and function like natural teeth.",
-        "image": "https://source.unsplash.com/random/250x250/?implants"
-      },
-      {
-        "name": "Dental Crowns",
-        "description": "Custom-made tooth-shaped caps to restore damaged or weak teeth.",
-        "image": "https://source.unsplash.com/random/250x250/?dentalcrown"
-      },
-      {
-        "name": "Root Canal Therapy",
-        "description": "Treatment to save and repair an infected or decayed tooth, avoiding extraction.",
-        "image": "https://source.unsplash.com/random/250x250/?rootcanal"
-      },
-      {
-        "name": "Dental Bridges",
-        "description": "Fixed prosthetic to replace missing teeth and restore your smile's appearance.",
-        "image": "https://source.unsplash.com/random/250x250/?dental"
-      },
-      {
-        "name": "Orthodontic Treatment",
-        "description": "Alignment of teeth using braces or aligners to improve the bite and aesthetics.",
-        "image": "https://source.unsplash.com/random/250x250/?Orthodontics"
-      }
-    ]
-  
+export default Treatments;
